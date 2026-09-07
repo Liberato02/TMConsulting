6b0413cbaaa5decf3db0b16a5f5dc0595e5a8bef
tecnando-rpa <pronandofinancas@gmail.com>
2026-09-07 18:53:55 -0300

    feat: adiciona rota coringa para capturar urls invalidas

--- soulup-app/src/App.tsx
+++ soulup-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
-import { RotaProtegida } from "./routes/RotaProtegida.tsx";
+import { RotaProtegida } from "./routes/RotaProtegida";
 import { MainLayout } from "./layouts/MainLayout";
 
 // Cada rota mora em sua própria pasta (src/routes/nome/index.tsx),
