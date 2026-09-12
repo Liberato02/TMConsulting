c7ae0939bcdaac4f25443411ecf5ac43b38743a7
tecnando-rpa <pronandofinancas@gmail.com>
2026-09-12 14:03:07 -0300

    feat: reestruturacao de pastas erro no nome, uso de routes ao inves de pages, conforme preve challenge

--- soulgame-academico/src/pages/home/index.tsx
+++ soulgame-academico/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useTituloDocumento } from "../../hooks/useTituloDocumento";
 
 const ETAPAS_ROADMAP = [
   "Pesquisa do desafio",
@@ -9,6 +10,8 @@ const ETAPAS_ROADMAP = [
 ];
 
 export function Home() {
+  useTituloDocumento("SoulGame | TM Consulting");
+
   return (
     <main aria-label="Página inicial do SoulGame" className="flex-1">
       {/* Hero */}
